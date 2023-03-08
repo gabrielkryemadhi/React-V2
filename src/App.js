faad190fa4997ea81f482257900931720da4898a
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-08 21:20:07 +0100

    Ex. done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-function App() {
-  return null
+class HelloWorld extends React.Component {
+ render(){
+  return <h1>Hello World!</h1>
+ }
 }
 
-export default App;
+export default HelloWorld;
