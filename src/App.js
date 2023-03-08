ba7a3661e23a00cbef14626655d7be6a7b4dc10d
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-08 22:55:41 +0100

    Ex. done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import HelloWorld from './Hello';
 
-export class App extends React.Component {
+class App extends React.Component {
     render(){
         return <div><HelloWorld /></div>
     }
 }
+
+export default App
