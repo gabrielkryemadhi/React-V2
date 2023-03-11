0ea8a7000011dc6c5e45d1ee55ec58f3046867f2
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-11 14:11:36 +0100

    Ex. done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Welcome from './welcome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Welcome name="Nela"/>
   </React.StrictMode>
 );
 
