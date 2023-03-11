68912ca94092fc3e3e915328fde7648aa9431eb6
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-11 15:16:00 +0100

    Ex. done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Welcome from './welcome';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Welcome age="897"/>
+    <Welcome name={<strong>Kela</strong>} age="897"/>
   </React.StrictMode>
 );
 
