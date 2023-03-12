162344593a1a4fb35a84f1915c59ed06dbc79e25
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-12 20:10:02 +0100

    Ex. done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Welcome from './welcome';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Welcome name={<strong>Kela</strong>} age="897"/>
+    <Welcome name={<strong>Kela</strong>} age="17"/>
   </React.StrictMode>
 );
-
--- src/welcome.js
+++ src/welcome.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Age } from "./age";
 
 class Welcome extends React.Component{
     render(){
         return (
         <div>
         <p>Hello, {this.props.name}!</p>
-        <p>Your age is {this.props.age}.</p>   
+        {this.props.age > 18 && <Age age={this.props.age}/>}  
         </div>
       )
     }
