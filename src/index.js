51e507e0343ae753b35b722e62ef380ed0d8f428
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-18 14:49:49 +0100

    Ex. done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Welcome from './welcome';
+import { Counter } from './counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Welcome name={<strong>John</strong>} age="10"/>
-  </React.StrictMode>
+     <Counter />
 );
