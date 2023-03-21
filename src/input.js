11bc243f99b5c5781302750b1cc1893df7e265a7
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-21 17:41:24 +0100

    Ex. done

--- src/input.js
+++ src/input.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Welcome } from "./welcome";
 
 export class Input extends React.Component{
-    state = { name: '' }
+    state = { 
+        username: '',
+        password: '',
+        remember: '',
+    }
 
     handleInput = (event) => {
         const value = event.target.value;
-        this.setState({ name: ' ' + value })
+        const name = event.target.name;
+        const type = event.target.type;
+        const checked = event.target.checked;
+
+        this.setState({ [name]: type === "checkbox" ? checked : value })
+    }
+
+    handleState = () => {
+        this.setState({
+            username: '',
+            password: '',
+            remember: '',
+        })
     }
 
     render() {
         return <>
-        <Welcome name={this.state.name}/>
-        <input name='nameOf' value={this.state.name} onChange={this.handleInput}></input>
+        <div>
+         <button onClick={this.handleState}><h2>Reset</h2></button>
+        </div>
+        <div>
+         <input name='username' value={this.state.username} onChange={this.handleInput}></input>
+         <input name='password' type='password' value={this.state.password} onChange={this.handleInput}></input>
+         <input name='remember' type='checkbox' checked={this.state.remember} onChange={this.handleInput}></input>
+         </div>
         </>
     }
 }
