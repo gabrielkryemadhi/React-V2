7ccf65b1c789320196bf7708ccf2538b80813197
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-27 19:46:20 +0200

    Ex. done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Container } from './container';
 import { Counter } from './state';
 import { Sum } from './sum';
 import { Welcome } from './welcome';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
      <>
+     <Container title="Component composition">
      <Welcome />
      <Sum />
      <Counter inicialValue={7} />
+     </Container>
      </>
 );
