40958c427c01b93565ecda06f001cc211c4a2391
Gabriel <Kryemadhigabriel@gmail.com>
2023-03-29 23:17:01 +0200

    Ex. done

--- src/state.js
+++ src/state.js
@@ -4,9 +4,9 @@ import { useState } from "react";
 export function Counter({inicialValue = 0}) {
     const [counter, setCounter] = useState(inicialValue)
 
-    function handleCounterIncrement(){
-        setCounter((s) => s + 1);
-    }
+    // function handleCounterIncrement(){
+    //     setCounter((s) => s + 1);
+    // }
 
     function handleCounterReset(){
         setCounter(inicialValue);
@@ -15,11 +15,19 @@ export function Counter({inicialValue = 0}) {
     useEffect(() => {
         console.log(`The counter is now: ${counter}`
         )}, [counter])
-     //the function should be passed as a prop, need help :)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(s => s + 1)
+        }, 1000)
+        return () => {
+            clearInterval(interval)}
+        }, [])
+
 
     return <>
     <h2>Counter: {counter}</h2>
-    <button onClick={handleCounterIncrement}>Increment</button>
+    {/* <button onClick={handleCounterIncrement}>Increment</button> */}
     <button onClick={handleCounterReset}>Reset</button>
     </>
 }
